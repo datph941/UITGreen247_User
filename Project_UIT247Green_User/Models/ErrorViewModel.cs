e705da14cd0466a5ea45ec7c1b2e14934c09147e
DangDo1364 <thaykv848565@gmail.com>
2021-11-26 14:42:17 +0700

    first commit

---
+++ Project_UIT247Green_User/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Project_UIT247Green_User.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
