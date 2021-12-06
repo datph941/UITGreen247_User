0265efe54d3743ca213f2a3c960aabefc51c6bd1
DangDo1364 <thaykv848565@gmail.com>
2021-12-06 22:48:15 +0700

    fix bug

---
+++ Project_UIT247Green_User/Controllers/CartController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_UIT247Green_User.Controllers
+{
+    public class CartController : Controller
+    {
+        public IActionResult Cart()
+        {
+            return View();
+        }
+    }
+}
---
+++ Project_UIT247Green_User/Controllers/CategoryController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_UIT247Green_User.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Category()
+        {
+            return View();
+        }
+    }
+}
