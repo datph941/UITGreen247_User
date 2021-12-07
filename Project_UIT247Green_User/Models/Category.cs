1854ff0765ad1e7f1fd52eb7802785860c3ccfea
DangDo1364 <thaykv848565@gmail.com>
2021-12-07 13:58:26 +0700

    commit model

---
+++ Project_UIT247Green_User/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_UIT247Green_User.Models
+{
+    public class Category
+    {
+        public int id_cat { set; get; }
+
+        public string name_cat { set; get; }
+
+        public int id_parent { set; get; }
+    }
+}
